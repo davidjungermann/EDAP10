bcf96faf903c72fa6b8dbe3635fe37989d889884
unknown <bas15jpe@student.lu.se>
2019-09-09 11:11:27 +0200

    timechangedone

--- lab1-alarmclock/src/ClockMain.java
+++ lab1-alarmclock/src/ClockMain.java
@@ -1,3 +1,4 @@
+import java.time.LocalTime;
 import java.util.concurrent.Semaphore;
 
 import clock.ClockInput;
@@ -8,33 +9,6 @@ import emulator.AlarmClockEmulator;
 
 public class ClockMain {
 
-<<<<<<< HEAD
-    public static void main(String[] args) throws InterruptedException {
-        AlarmClockEmulator emulator = new AlarmClockEmulator();
-
-        ClockInput  in  = emulator.getInput();
-        ClockOutput out = emulator.getOutput();
-        
-
-        out.displayTime(1337);   // arbitrary time: just an example
-
-        Semaphore sem = in.getSemaphore();
-        Semaphore updateTime = new Semaphore(1);
-        clockTicker ct = new clockTicker(out, updateTime, in);
-        Thread clockCounter = new Thread(ct);
-        //clockCounter.start();
-        
-        while (true) {
-            sem.acquire();                       // wait for user input
-            UserInput userInput = in.getUserInput();
-            int choice = userInput.getChoice();
-            int value = userInput.getValue();
-            int i = 0244;
-            System.out.println("choice = " + choice + "  value=" + value);
-            System.out.println(i);
-        }
-    }
-=======
 	public static void main(String[] args) throws InterruptedException {
 		AlarmClockEmulator emulator = new AlarmClockEmulator();
 
@@ -43,7 +17,7 @@ public class ClockMain {
 
 		Semaphore sem = in.getSemaphore();
 		Semaphore mutex = new Semaphore(1);
-		ClockTicker ct = new ClockTicker(out, in, mutex);
+		ClockTicker ct = new ClockTicker(out, in, mutex, LocalTime.now());
 		Thread clockCounter = new Thread(ct);
 		clockCounter.start();
 
@@ -57,8 +31,10 @@ public class ClockMain {
 			int mm = value % 100;
 			value /= 100;
 			int hh = value % 100;
-
-			//System.out.println("choice = " + choice + " value=" + value);
+			if (choice == 1 && value != 999999) {
+				ct.setTime(LocalTime.of(hh, mm, ss));
+			}
+			System.out.println("choice = " + choice + " value=" + value);
 			System.out.println("HH: " + hh);
 			System.out.println("MM: " + mm);
 			System.out.println("SS: " + ss);
@@ -66,5 +42,4 @@ public class ClockMain {
 
 		}
 	}
->>>>>>> 33c24ec30146fb0b9dd1079c2693e79964561b13
 }
--- lab1-alarmclock/src/clock/ClockTicker.java
+++ lab1-alarmclock/src/clock/ClockTicker.java
@@ -8,39 +8,56 @@ public class ClockTicker implements Runnable {
 	private ClockOutput out;
 	private Semaphore mutex;
 	private ClockInput in;
-
-	public ClockTicker(ClockOutput out, ClockInput in, Semaphore mutex) {
+	private LocalTime time;
+	
+	public ClockTicker(ClockOutput out, ClockInput in, Semaphore mutex, LocalTime time) {
 		this.in = in;
 		this.out = out;
 		this.mutex = mutex;
+		this.time = time;
 	}
 
 	public ClockTicker() {
 
+	}
+	
+	public void setTime(LocalTime time){
+		try {
+			mutex.acquire();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		this.time = time;
+		mutex.release();
 	}
 
 	public void run() {
 		long intervalTime = 1000;
 		long startTime = System.currentTimeMillis();
 		long sleepTime;
-
 		while (true) {
-			String timeValue = LocalTime.now().toString().replace(":", "");
-			timeValue = timeValue.substring(0, timeValue.indexOf("."));
+			
 			try {
 				mutex.acquire();
-
+				
 			} catch (InterruptedException e) {
 				e.printStackTrace();
+			}
+			String timeValue = time.toString().replace(":", "");
+			if(timeValue.contains(".")){
+				
+				timeValue = timeValue.substring(0, timeValue.indexOf("."));
 			}
 			mutex.release();
 			sleepTime = (intervalTime - (System.currentTimeMillis() - startTime) % intervalTime);
 			try {
 				Thread.sleep(sleepTime);
 				out.displayTime((Integer.parseInt(timeValue)));
+				time = time.plusSeconds(1);	
 			} catch (InterruptedException e1) {
 				e1.printStackTrace();
 			}
+			
 		}
 
 	}
