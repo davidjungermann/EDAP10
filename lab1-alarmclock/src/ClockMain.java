7f1ddb717831e2ac774d2a6481ca82cb56733b6f
ravedave <dic15dju@student.lu.se>
2019-09-09 13:18:10 +0200

    format

--- lab1-alarmclock/src/ClockMain.java
+++ lab1-alarmclock/src/ClockMain.java
@@ -12,10 +12,8 @@ public class ClockMain {
 
 	public static void main(String[] args) throws InterruptedException {
 		AlarmClockEmulator emulator = new AlarmClockEmulator();
-
 		ClockInput in = emulator.getInput();
 		ClockOutput out = emulator.getOutput();
-
 		Semaphore sem = in.getSemaphore();
 		Semaphore mutex = new Semaphore(1);
 		TimeState timeState = new TimeState(mutex);
@@ -33,14 +31,14 @@ public class ClockMain {
 			int mm = value % 100;
 			value /= 100;
 			int hh = value % 100;
-			if (choice == 1 && value != 999999) {		
+			
+			if (choice == 1 && value != 999999) {
 				timeState.setTime(LocalTime.of(hh, mm, ss));
 			}
 			System.out.println("choice = " + choice + " value=" + value);
 			System.out.println("HH: " + hh);
 			System.out.println("MM: " + mm);
 			System.out.println("SS: " + ss);
-			
 
 		}
 	}
--- lab1-alarmclock/src/clock/ClockTicker.java
+++ lab1-alarmclock/src/clock/ClockTicker.java
@@ -9,7 +9,7 @@ public class ClockTicker implements Runnable {
 	private Semaphore mutex;
 	private ClockInput in;
 	private TimeState timeState;
-	
+
 	public ClockTicker(ClockOutput out, ClockInput in, Semaphore mutex, TimeState timeState) {
 		this.in = in;
 		this.out = out;
@@ -20,32 +20,29 @@ public class ClockTicker implements Runnable {
 	public ClockTicker() {
 
 	}
-	
 
 	public void run() {
 		long intervalTime = 1000;
 		long startTime = System.currentTimeMillis();
 		long sleepTime;
 		while (true) {
-			
+
 			sleepTime = (intervalTime - (System.currentTimeMillis() - startTime) % intervalTime);
 			try {
 				Thread.sleep(sleepTime);
-			String timeValue = timeState.getTime().toString().replace(":", "");
-			if(timeValue.length() == 4){
-				timeValue = timeValue + "00";
-			}
-			if(timeValue.contains(".")){
-				
-				timeValue = timeValue.substring(0, timeValue.indexOf("."));
-			}
-
+				String timeValue = timeState.getTime().toString().replace(":", "");
+				if (timeValue.length() == 4) {
+					timeValue = timeValue + "00";
+				}
+				if (timeValue.contains(".")) {
+					timeValue = timeValue.substring(0, timeValue.indexOf("."));
+				}
 				out.displayTime((Integer.parseInt(timeValue)));
 				timeState.setTime(timeState.getTime().plusSeconds(1));
 			} catch (InterruptedException e1) {
 				e1.printStackTrace();
 			}
-			
+
 		}
 
 	}
