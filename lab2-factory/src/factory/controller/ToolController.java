63df7780a3b41c38f6bcb1e293167e502aa88001
ravedave <dic15dju@student.lu.se>
2019-09-12 16:08:21 +0200

    Lab2 påbörjad. Klar till och med I6

--- lab2-factory/src/factory/controller/ToolController.java
+++ lab2-factory/src/factory/controller/ToolController.java
@@ -5,49 +5,46 @@ import factory.model.WidgetKind;
 import factory.swingview.Factory;
 
 public class ToolController {
-    private final DigitalSignal conveyor, press, paint;
-    private final long pressingMillis, paintingMillis;
-    
-    public ToolController(DigitalSignal conveyor,
-                          DigitalSignal press,
-                          DigitalSignal paint,
-                          long pressingMillis,
-                          long paintingMillis)
-    {
-        this.conveyor = conveyor;
-        this.press = press;
-        this.paint = paint;
-        this.pressingMillis = pressingMillis;
-        this.paintingMillis = paintingMillis;
-    }
+  private final DigitalSignal conveyor, press, paint;
+  private final long pressingMillis, paintingMillis;
+  private boolean isPressing;
+  private boolean isPainting;
 
-    public void onPressSensorHigh(WidgetKind widgetKind) throws InterruptedException {
-        //
-        // TODO: you will need to modify this method
-        //
-        if (widgetKind == WidgetKind.BLUE_RECTANGULAR_WIDGET) {
-            conveyor.off();
-            press.on();
-            Thread.sleep(pressingMillis);
-            press.off();
-            Thread.sleep(pressingMillis);   // press needs this time to retract
-            conveyor.on();
-        }
-    }
+  public ToolController(DigitalSignal conveyor, DigitalSignal press, DigitalSignal paint, long pressingMillis,
+      long paintingMillis) {
+    this.conveyor = conveyor;
+    this.press = press;
+    this.paint = paint;
+    this.pressingMillis = pressingMillis;
+    this.paintingMillis = paintingMillis;
+  }
 
-    public void onPaintSensorHigh(WidgetKind widgetKind) throws InterruptedException {
-        //
-        // TODO: you will need to modify this method
-        //
-        if (widgetKind == WidgetKind.ORANGE_ROUND_WIDGET) {
-        	// TODO
-        }
+  public synchronized void onPressSensorHigh(WidgetKind widgetKind) throws InterruptedException {
+    if (widgetKind == WidgetKind.BLUE_RECTANGULAR_WIDGET) {
+      conveyor.off();
+      press.on();
+      Thread.sleep(pressingMillis);
+      press.off();
+      Thread.sleep(pressingMillis); // press needs this time to retract
+      conveyor.on();
     }
-    
-    // -----------------------------------------------------------------------
-    
-    public static void main(String[] args) {
-        Factory factory = new Factory();
-        factory.startSimulation();
+  }
+
+  public synchronized void onPaintSensorHigh(WidgetKind widgetKind) throws InterruptedException {
+    if (widgetKind == WidgetKind.ORANGE_ROUND_WIDGET) {
+      conveyor.off();
+      paint.on();
+      Thread.sleep(paintingMillis);
+      paint.off();
+      Thread.sleep(paintingMillis);
+      conveyor.on();
     }
+  }
+
+  // -----------------------------------------------------------------------
+
+  public static void main(String[] args) {
+    Factory factory = new Factory();
+    factory.startSimulation();
+  }
 }
