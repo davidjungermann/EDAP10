36ef4e368256dcbd590cdd4394740c30c878fe6d
ravedave <dic15dju@student.lu.se>
2019-09-30 14:19:45 +0200

    I4

--- lab4-primefac/src/CodeBreaker.java
+++ lab4-primefac/src/CodeBreaker.java
@@ -1,8 +1,3 @@
-
-
-import java.awt.Event;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.math.BigInteger;
 
 import javax.swing.JButton;
@@ -14,64 +9,89 @@ import client.view.StatusWindow;
 import client.view.WorklistItem;
 import network.Sniffer;
 import network.SnifferCallback;
+import rsa.Factorizer;
+import rsa.ProgressTracker;
 
 public class CodeBreaker implements SnifferCallback {
 
-    private final JPanel workList;
-    private final JPanel progressList;
-    
-    private final JProgressBar mainProgressBar;
+  private final JPanel workList;
+  private final JPanel progressList;
 
-    // -----------------------------------------------------------------------
-    
-    private CodeBreaker() {
-        StatusWindow w  = new StatusWindow();
+  private final JProgressBar mainProgressBar;
 
-        workList        = w.getWorkList();
-        progressList    = w.getProgressList();
-        mainProgressBar = w.getProgressBar();
-        
-        new Sniffer(this).start();
-    }
-    
-    // -----------------------------------------------------------------------
-    
-    public static void main(String[] args) throws Exception {
+  // -----------------------------------------------------------------------
 
-        /*
-         * Most Swing operations (such as creating view elements) must be
-         * performed in the Swing EDT (Event Dispatch Thread).
-         * 
-         * That's what SwingUtilities.invokeLater is for.
-         */
+  private CodeBreaker() {
+    StatusWindow w = new StatusWindow();
 
-        SwingUtilities.invokeLater(() -> new CodeBreaker());
-    }
+    workList = w.getWorkList();
+    progressList = w.getProgressList();
+    mainProgressBar = w.getProgressBar();
 
-    // -----------------------------------------------------------------------
+    new Sniffer(this).start();
+  }
 
-    /** Called by a Sniffer thread when an encrypted message is obtained. */
+  // -----------------------------------------------------------------------
+
+  public static void main(String[] args) throws Exception {
+
+    /*
+     * Most Swing operations (such as creating view elements) must be performed in
+     * the Swing EDT (Event Dispatch Thread).
+     * 
+     * That's what SwingUtilities.invokeLater is for.
+     */
+
+    SwingUtilities.invokeLater(() -> new CodeBreaker());
+  }
+
+  // -----------------------------------------------------------------------
+
+  /** Called by a Sniffer thread when an encrypted message is obtained. */
+  @Override
+  public void onMessageIntercepted(String message, BigInteger n) {
+    // System.out.println("message intercepted (N=" + n + ")...");
+    WorklistItem workListItem = new WorklistItem(n, "N: " + n);
+    JButton button = new JButton("LET'S GET CRACKIN'");
+    ProgressTracker tracker = new Tracker();
+    button.addActionListener(e -> {
+      moveItem(workListItem, button);
+      startCrackinG(message, n, tracker);
+    });
+    workListItem.add(button);
+    workList.add(workListItem);
+  }
+
+  private void startCrackinG(String code, BigInteger n, ProgressTracker tracker) {
+    Factorizer.crack(code, n, tracker);
+  }
+
+  private void moveItem(WorklistItem workListItem, JButton button) {
+    workListItem.remove(workListItem);
+    progressList.add(workListItem);
+    workListItem.remove(button);
+  }
+
+  private static class Tracker implements ProgressTracker {
+    private int totalProgress = 0;
+    private int prevPercent = -1;
+
+    /**
+     * Called by Factorizer to indicate progress. The total sum of ppmDelta from all
+     * calls will add upp to 1000000 (one million).
+     * 
+     * @param ppmDelta portion of work done since last call, measured in ppm (parts
+     *                 per million)
+     */
     @Override
-    public void onMessageIntercepted(String message, BigInteger n) {
-        //System.out.println("message intercepted (N=" + n + ")...");
-    	WorklistItem workListItem = new WorklistItem(n, "N: " + n);
-    	JButton button = new JButton("LET'S GET CRACKIN'");
-    	button.addActionListener(e -> {
-    		moveItem(workListItem, button);
-    		startCrackinG(n);
-    	});
-    	workListItem.add(button);
-        workList.add(workListItem);
+    public void onProgress(int ppmDelta) {
+      totalProgress += ppmDelta;
+      int percent = totalProgress / 10000;
+      if (percent != prevPercent) {
+        System.out.println(percent + "%");
+        prevPercent = percent;
+      }
     }
-    
-    private void startCrackinG(BigInteger n){
-    	
-    	
-    }
-    
-    private void moveItem(WorklistItem workListItem, JButton button){
-    	workListItem.remove(workListItem);
-    	progressList.add(workListItem);
-    	workListItem.remove(button);
-    }
+  }
+
 }
