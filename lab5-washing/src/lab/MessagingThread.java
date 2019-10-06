c07dd74f08150c79df609515d085b023352f0d98
ravedave <dic15dju@student.lu.se>
2019-10-06 13:21:29 +0200

    unit test pass

--- lab5-washing/src/lab/MessagingThread.java
+++ lab5-washing/src/lab/MessagingThread.java
@@ -1,23 +1,33 @@
 package lab;
 
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.TimeUnit;
+
 public class MessagingThread<M> extends Thread {
 
-    /** Called by another thread, to send a message to this thread. */
-    public void send(M message) {
-        // TODO: implement this method (one or a few lines)
-    }
-    
-    /** Returns the first message in the queue, or blocks if none available. */
-    protected M receive() throws InterruptedException {
-        // TODO: implement this method (one or a few lines)
-        return null;
-    }
-    
-    /** Returns the first message in the queue, or blocks up to 'timeout'
-        milliseconds if none available. Returns null if no message is obtained
-        within 'timeout' milliseconds. */
-    protected M receiveWithTimeout(long timeout) throws InterruptedException {
-        // TODO: implement this method (one or a few lines)
-        return null;
-    }
+  private BlockingQueue<M> queue = new LinkedBlockingDeque<>();
+
+  /**
+   * Called by another thread, to send a message to this thread.
+   * 
+   * @throws InterruptedException
+   */
+  public void send(M message) throws InterruptedException {
+    queue.put(message);
+  }
+
+  /** Returns the first message in the queue, or blocks if none available. */
+  protected M receive() throws InterruptedException {
+    return queue.take();
+  }
+
+  /**
+   * Returns the first message in the queue, or blocks up to 'timeout'
+   * milliseconds if none available. Returns null if no message is obtained within
+   * 'timeout' milliseconds.
+   */
+  protected M receiveWithTimeout(long timeout) throws InterruptedException {
+    return queue.poll(timeout, TimeUnit.MILLISECONDS);
+  }
 }
