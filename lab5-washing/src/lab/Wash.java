edf800628f2dab7f3e48b141d41e662be7c4c9af
ravedave <dic15dju@student.lu.se>
2019-10-06 14:33:57 +0200

    switch case for program handling

--- lab5-washing/src/lab/Wash.java
+++ lab5-washing/src/lab/Wash.java
@@ -8,6 +8,7 @@ public class Wash {
   // simulation speed-up factor:
   // 50 means the simulation is 50 times faster than real time
   public static final int SPEEDUP = 50;
+  private static Thread currentThread;
 
   public static void main(String[] args) throws InterruptedException {
     WashingSimulator sim = new WashingSimulator(SPEEDUP);
@@ -25,11 +26,17 @@ public class Wash {
     while (true) {
       int n = io.awaitButton();
       System.out.println("user selected program " + n);
-      
-      if(n == 3) {
-        WashingProgram3 washingProgram3 = new WashingProgram3(io, temp, water, spin);
-        washingProgram3.run();
+
+      switch (n) {
+      case 0:
+        currentThread.interrupt();
+        break;
+        
+      case 3:
+        currentThread = new WashingProgram3(io, temp, water, spin);
+        currentThread.start();
       }
+
 
       // TODO:
       // if the user presses buttons 1-3, start a washing program
