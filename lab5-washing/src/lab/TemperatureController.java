b6a64668dd78cbc8f7bd1b7ad263f4bb64c6b703
unknown <bas15jpe@student.lu.se>
2019-10-07 15:51:57 +0200

    tja

--- lab5-washing/src/lab/TemperatureController.java
+++ lab5-washing/src/lab/TemperatureController.java
@@ -4,14 +4,39 @@ import wash.WashingIO;
 
 public class TemperatureController extends MessagingThread<WashingMessage> {
 
-    // TODO: add attributes
+	WashingIO io;
+	long dt;
+	double mu;
+	double ml;
 
-    public TemperatureController(WashingIO io) {
-        // TODO
-    }
+	public TemperatureController(WashingIO io) {
+		this.io = io;
+		this.dt = 10000;
+		this.mu = 0.678;
+		this.ml = 0.19048;
+	}
 
-    @Override
-    public void run() {
-        // TODO
-    }
+	@Override
+	public void run() {
+		while (true) {
+			try {
+				WashingMessage m = receive();
+				if (m.getCommand() == m.TEMP_SET) {
+					double goalTemp = m.getValue();
+					while (true) {
+						if (io.getTemperature() > goalTemp - mu) {
+							io.heat(false);
+						}
+						if (io.getTemperature() < goalTemp + ml) {
+							io.heat(true);
+						}
+						this.sleep(dt);
+					}
+				}
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
 }
--- lab5-washing/src/lab/WaterController.java
+++ lab5-washing/src/lab/WaterController.java
@@ -4,14 +4,36 @@ import wash.WashingIO;
 
 public class WaterController extends MessagingThread<WashingMessage> {
 
-    // TODO: add attributes
+	WashingIO io;
 
-    public WaterController(WashingIO io) {
-        // TODO
-    }
+	public WaterController(WashingIO io) {
+		this.io = io;
+	}
 
-    @Override
-    public void run() {
-        // TODO
-    }
+	@Override
+	public void run() {
+		while (true) {
+
+			try {
+				
+				WashingMessage m = receive();
+				
+				while (m != null) {
+					
+					if (m.getCommand() == WashingMessage.WATER_FILL) {
+						io.lock(true);
+						io.fill(true);
+						this.sleep(2000);
+						io.fill(false);
+						//io.fill(false);
+						m = null;
+						break;
+					}
+				}
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
 }
