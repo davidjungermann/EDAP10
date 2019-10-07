20c960c1fac9189d9e781a90ad521fb6f104f665
ravedave <dic15dju@student.lu.se>
2019-10-07 15:51:57 +0200

    test

--- lab5-washing/src/lab/TemperatureController.java
+++ lab5-washing/src/lab/TemperatureController.java
@@ -4,14 +4,62 @@ import wash.WashingIO;
 
 public class TemperatureController extends MessagingThread<WashingMessage> {
 
-    // TODO: add attributes
+  private WashingIO io;
+  private static final int dt = 10000;
+  private static final double mu = 0.678;
+  private static final double ml = -0.19048;
 
-    public TemperatureController(WashingIO io) {
-        // TODO
-    }
+  private double currentTemp;
+  private double wantedTemp;
+  private double upperBound;
+  private double lowerBound;
 
-    @Override
-    public void run() {
-        // TODO
+  public TemperatureController(WashingIO io) {
+    this.io = io;
+  }
+
+  @Override
+  public void run() {
+    try {
+      while (true) {
+        WashingMessage m = receive();
+        while (m != null) {
+          if (m.getCommand() == WashingMessage.TEMP_IDLE) {
+            m = null;
+            break;
+          }
+
+          if (m.getCommand() == WashingMessage.TEMP_SET) {
+            while (true) {
+              m = receiveWithTimeout(dt / Wash.SPEEDUP);
+              if (m != null) {
+                wantedTemp = m.getValue();
+                upperBound = wantedTemp - mu;
+                lowerBound = wantedTemp + ml;
+              } else {
+                io.heat(false);
+                break;
+              }
+            }
+            currentTemp = io.getTemperature();
+
+            if (lowerBound >= currentTemp) {
+              io.heat(true);
+            } else if (upperBound <= currentTemp) {
+              io.heat(false);
+            }
+            if (currentTemp >= wantedTemp - 2 && currentTemp < wantedTemp) {
+              System.out.println(currentTemp + "Call: Temperature regulation");
+              send(new WashingMessage(this, WashingMessage.ACKNOWLEDGMENT));
+            }
+          }
+          break;
+        }
+      }
+    } catch (InterruptedException unexpected) {
+      // we don't expect this thread to be interrupted,
+      // so throw an error if it happens anyway
+      throw new Error(unexpected);
     }
+  }
 }
--- lab5-washing/src/lab/WaterController.java
+++ lab5-washing/src/lab/WaterController.java
@@ -4,14 +4,62 @@ import wash.WashingIO;
 
 public class WaterController extends MessagingThread<WashingMessage> {
 
-    // TODO: add attributes
+  private WashingIO io;
+  private static final int dt = 100;
+  private static final double mu = 0.678;
+  private static final double ml = -0.19048;
 
-    public WaterController(WashingIO io) {
-        // TODO
-    }
+  private double currentLevel;
+  private double wantedLevel;
+  private double upperBound;
+  private double lowerBound;
 
-    @Override
-    public void run() {
-        // TODO
+  public WaterController(WashingIO io) {
+    this.io = io;
+  }
+
+  @Override
+  public void run() {
+    try {
+      while (true) {
+        WashingMessage m = receive();
+        while (m != null) {
+          if (m.getCommand() == WashingMessage.WATER_IDLE) {
+            m = null;
+            break;
+          }
+
+          if (m.getCommand() == WashingMessage.WATER_FILL) {
+            while (true) {
+              m = receiveWithTimeout(dt / Wash.SPEEDUP);
+              if (m != null) {
+                wantedTemp = m.getValue();
+                upperBound = wantedTemp - mu;
+                lowerBound = wantedTemp + ml;
+              } else {
+                io.heat(false);
+                break;
+              }
+            }
+            currentTemp = io.getTemperature();
+
+            if (lowerBound >= currentTemp) {
+              io.heat(true);
+            } else if (upperBound <= currentTemp) {
+              io.heat(false);
+            }
+            if (currentTemp >= wantedTemp - 2 && currentTemp < wantedTemp) {
+              System.out.println(currentTemp + "Call: Temperature regulation");
+              send(new WashingMessage(this, WashingMessage.ACKNOWLEDGMENT));
+            }
+          }
+          break;
+        }
+      }
+    } catch (InterruptedException unexpected) {
+      // we don't expect this thread to be interrupted,
+      // so throw an error if it happens anyway
+      throw new Error(unexpected);
     }
+  }
 }
